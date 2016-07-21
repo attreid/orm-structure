a428414c5ed71cc6528b3e930d47426ea554c182
Attreid <attreid@gmail.com>
2016-07-21 10:27:10 +0200

    update

---
+++ src/Repository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NAttreid\Orm;
+
+/**
+ * Repository
+ *
+ * @author Attreid <attreid@gmail.com>
+ */
+abstract class Repository extends \Nextras\Orm\Repository\Repository {
+    
+}
