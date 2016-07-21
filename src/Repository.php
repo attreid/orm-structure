a5d9dfa90e93fd958392f01a0aeb9d5c47389d94
Attreid <attreid@gmail.com>
2016-07-21 10:32:11 +0200

    update

--- src/Repository.php
+++ src/Repository.php
@@ -8,5 +8,21 @@ namespace NAttreid\Orm;
  * @author Attreid <attreid@gmail.com>
  */
 abstract class Repository extends \Nextras\Orm\Repository\Repository {
-    
+
+    /**
+     * Vrati pole [id, name] serazene podle [name]
+     * @return array
+     */
+    public function fetchPairsByName() {
+        return $this->findAll()->orderBy('name')->fetchPairs('id', 'name');
+    }
+
+    /**
+     * Vrati pole [id, name] serazene podle [id]
+     * @return array
+     */
+    public function fetchPairsById() {
+        return $this->findAll()->orderBy('id')->fetchPairs('id', 'name');
+    }
+
 }
